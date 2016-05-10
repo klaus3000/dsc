e0e446210f9a7b9b86925fa41aee7c2657e6a2e2
Klaus Darilion <klaus.darilion@nic.at>
2016-05-10 11:03:51 +0000

    new config option 'statistics_interval' (defaults to 60s)
    Defines how often DSC writes statistics. Patch by michael.braunoeder@nic.at.
    
    (cherry picked from commit 3cf5a315081dd34bb244aa1b924251b43c52adee)

--- src/ParseConfig.cc
+++ src/ParseConfig.cc
@@ -61,6 +61,7 @@ int open_interface(const char *);
 int set_run_dir(const char *);
 int set_minfree_bytes(const char *);
 int set_pid_file(const char *);
+int set_statistics_interval(const char *);
 int add_dataset(const char *name, const char *layer,
     const char *firstname, const char *firstindexer,
     const char *secondname, const char *secondindexer,
@@ -100,6 +101,7 @@ Rule rInterface("Interface", 0);
 Rule rRunDir("RunDir", 0);
 Rule rMinfreeBytes("MinfreeBytes", 0);
 Rule rPidFile("PidFile", 0);
+Rule rStatisticsInterval("StatisticsInterval",0);
 Rule rLocalAddr("LocalAddr", 0);
 Rule rPacketFilterProg("PacketFilterProg", 0);
 Rule rDatasetOpt("DatasetOpt", 0);
@@ -170,6 +172,13 @@ interpret(const Pree &tree, int level)
 		assert(tree.count() > 1);
                 if (set_pid_file(remove_quotes(tree[1].image()).c_str()) != 1) {
 			cerr << "interpret() failure in pid_file" << endl;
+			return 0;
+		}
+	} else
+	if (tree.rid() == rStatisticsInterval.id()) {
+		assert(tree.count() > 1);
+		if (set_statistics_interval(remove_quotes(tree[1].image()).c_str()) != 1) {
+			cerr << "interpret() failures in statistics_interval" << endl;
 			return 0;
 		}
 	} else
@@ -392,6 +401,7 @@ ParseConfig(const char *fn)
 	rRunDir = "run_dir" >>rQuotedToken >>";" ;
 	rMinfreeBytes = "minfree_bytes" >>rDecimalNumber >>";" ;
 	rPidFile = "pid_file" >>rQuotedToken >>";" ;
+	rStatisticsInterval = "statistics_interval" >>rDecimalNumber >> ";" ;
 	rLocalAddr = "local_address" >>rIPAddress >>";" ;
 	rPacketFilterProg = "bpf_program" >>rQuotedToken >>";" ;
 	rDatasetOpt = rBareToken >> "=" >> rDecimalNumber;
@@ -416,6 +426,7 @@ ParseConfig(const char *fn)
 		rRunDir |
 		rMinfreeBytes |
 		rPidFile |
+		rStatisticsInterval |
 		rLocalAddr |
 		rPacketFilterProg |
 		rDataset |
